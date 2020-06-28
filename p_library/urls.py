4a35d19d61dce35760a252cc69782f4ea3556fcc
Vadim Basov <basvad@outlook.com>
2020-06-28 21:59:45 +0300

    D6-2

---
+++ p_library/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin  
+from django.urls import path  
+from .views import AuthorEdit, AuthorList, BookList, author_create_many, books_authors_create_many,FriendEdit, FriendList
+
+app_name = 'p_library'  
+"""urlpatterns = [  
+  path('author/create', AuthorEdit.as_view(), name='author_create'),  
+  path('authors', AuthorList.as_view(), name='author_list'),
+  path('author/create_many', author_create_many, name='author_create_many'),
+  path('author_book/create_many', books_authors_create_many, name='author_book_create_many'),  
+]"""
+
+urlpatterns = [
+        path('author/create', AuthorEdit.as_view(), name='author_create'),
+        path('authors', AuthorList.as_view(), name='author_list'),
+        path('books', BookList.as_view(), name='book_list'),
+        path('friends/create', FriendEdit.as_view(), name='friend_create'),
+        path('friends', FriendList.as_view(), name='friend_list'),
+        path('author/create_many', author_create_many, name='author_create_many'),
+        path('author_book/create_many', books_authors_create_many, name='author_book_create_many'),
+]
